6cf1b344d5ed89bf067e56a8224660f02359f3bf
Luis Vice <khanccer@gmail.com>
2020-09-11 13:52:49 -0500

    peliculas v1.8

---
+++ lib/models/actor_model.dart
@@ -0,0 +1,43 @@
+
+class Actor {
+  int castId;
+  String character;
+  String creditId;
+  int gender;
+  int id;
+  String name;
+  int order;
+  String profilePath;
+
+  Actor({
+      this.castId,
+      this.character,
+      this.creditId,
+      this.gender,
+      this.id,
+      this.name,
+      this.order,
+      this.profilePath,
+  });
+
+  Actor.fromJsonMap( Map<String, dynamic> json) {
+    castId      = json['cast_id'];
+    character   = json['character'];
+    creditId    = json['credit_id'];
+    gender      = json['gender'];
+    id          = json['id'];
+    name        = json['name'];
+    order       = json['order'];
+    profilePath = json['profile_path'];
+  }
+
+  getPosterImg() {
+    if ( profilePath == null) {
+      return 'https://bizraise.pro/wp-content/uploads/2014/09/no-avatar-300x300.png';
+    }
+    else {
+      return 'https://image.tmdb.org/t/p/w500/$profilePath';
+    }
+  }
+
+}
