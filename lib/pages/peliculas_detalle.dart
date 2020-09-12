2b96f7326835e671c396538a51fa6977910a516f
Luis Vice <khanccer@gmail.com>
2020-09-11 20:18:17 -0500

    peliculas v2.0

--- lib/pages/peliculas_detalle.dart
+++ lib/pages/peliculas_detalle.dart
@@ -84,11 +84,14 @@ class PeliculaDetalle extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 20.0),
       child: Row(
         children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Image(
-              image: NetworkImage( pelicula.getPosterImg() ),
-              height: 150.0,
+          Hero(
+            tag: pelicula.id,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image(
+                image: NetworkImage( pelicula.getPosterImg() ),
+                height: 150.0,
+              ),
             ),
           ),
           SizedBox(width: 20.0,),
--- lib/widgets/movie_horizontal.dart
+++ lib/widgets/movie_horizontal.dart
@@ -40,13 +40,16 @@ class MovieHorizontal extends StatelessWidget {
       margin: EdgeInsets.only(right: 15.0),
       child: Column(
         children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: FadeInImage(
-              image: NetworkImage( pelicula.getPosterImg() ),
-              placeholder: AssetImage('assets/no-image.jpg'),
-              fit: BoxFit.cover,
-              height: 110.0,
+          Hero(
+            tag: pelicula.id,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: FadeInImage(
+                image: NetworkImage( pelicula.getPosterImg() ),
+                placeholder: AssetImage('assets/no-image.jpg'),
+                fit: BoxFit.cover,
+                height: 110.0,
+              ),
             ),
           ),
           SizedBox(height: 5.0),
